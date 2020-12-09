d83f22f30e7e634de0fa129cad1d9b1de33d985a
Michelle Ranly <meranly8@gmail.com>
2020-12-09 07:43:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(email: params[:email], password: params[:password])
   end
 
   get '/account' do
@@ -24,4 +24,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
